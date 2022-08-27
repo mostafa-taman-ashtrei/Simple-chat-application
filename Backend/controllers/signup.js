4c96cb17d04360252444bbe8440181c6e9012ea7
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-08-27 11:48:53 +0530

    controller, middleware and server

---
+++ Backend/controllers/signup.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const signupModel = require("../models/signupSchema.js");
+
+router.post("/", async (req, res) => {
+  var data = new signupModel({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave)
+  } catch(error) {
+    res.status(400).json({message: error.message});
+  }
+});
+
+module.exports=router
---
+++ Backend/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require('body-parser');
+var signinRoute = require("./controllers/signin.js");
+var signupRoute = require("./controllers/signup.js");
+
+var app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Database connection
+mongoose.connect(process.env.DB_CONNECTION_STRING);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+// app.use("/signin", signinRoute);
+app.use("/signup", signupRoute);
+
+app.listen(3000, function () {
+  console.log("Working on 3000!!!");
+});
