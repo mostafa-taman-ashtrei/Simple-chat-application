c58f23c7b0de8fea27c1d2749ec1f755a6117219
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-08-27 11:49:44 +0530

    Arsalan (#2)

---
+++ Backend/models/signupSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const signupSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Enter your name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Enter your email"]
+    },
+    password: {
+        type: String,
+        required: [true, "Enter your password"]
+    },
+});
+
+module.exports = mongoose.model("signupModel", signupSchema);
