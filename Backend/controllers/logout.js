b5e8ea042dd72c4779b0126a841b3859c5fcb6e0
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-08-28 12:18:34 +0530

    logout added

---
+++ Backend/controllers/logout.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.cookie('token', "loggedOut");
+
+    res.status(200).json({message: "successfully logged out"})
+})
+
+module.exports = router;
